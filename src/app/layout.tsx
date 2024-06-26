8f278b4a4b8e7fc02191a418be85ff2536811225
Fang Jin <windmaomao@gmail.com>
2024-06-26 07:23:21 -0400

    add home page content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Driveway Services Demo",
@@ -16,7 +13,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <main className="flex min-h-screen flex-col items-center justify-between">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import Link from "next/link";
+import Image from "next/image";
+import servicePic from "../../public/services.jpg";
+import styles from "./page.module.css";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/services">Services</Link>
+    <div className={styles.content}>
+      <h1 className={styles.title}>Welcome to Lithia Motors Service!</h1>
+      <Image
+        src={servicePic}
+        alt="Services"
+        placeholder="blur"
+        priority={false}
+        width={460}
+        height={328}
+      />
+      <p className={styles.description}>
+        Lithia motors wants to put you in full control of your car-owning
+        experience by providing eat to book service appointments from the
+        comfort of your own home!
+      </p>
+      <button className={styles.action}>
+        <Link href="/services">Get Started</Link>
+      </button>
     </div>
   );
 }
